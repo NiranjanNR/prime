4d467e27431807d41536b3e465b6639d6848d5f2
NiranjanNR <kidkool8@gmail.com>
2024-02-09 03:59:47 -0800

    ui added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { Card, CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, ButtonModule, CardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
